beb1ed3ff834342ef113f4eff690a0aa4925ce14
giorgioGunawan <grggnwn@gmail.com>
2020-05-24 19:34:08 +1000

    Updates

---
+++ precache-manifest.3eacc69d74e141bf5fb6d8b74d9c3d01.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "82695c005e58a44309faef16e882c7d1",
+    "url": "/ShlackChat/index.html"
+  },
+  {
+    "revision": "19a8a0d543641fc0c98e",
+    "url": "/ShlackChat/static/css/main.eab24d3c.chunk.css"
+  },
+  {
+    "revision": "bccc048ded98aa94c977",
+    "url": "/ShlackChat/static/js/2.469f687e.chunk.js"
+  },
+  {
+    "revision": "3be25bf51018a491ef7f73dfd2dd74c4",
+    "url": "/ShlackChat/static/js/2.469f687e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "19a8a0d543641fc0c98e",
+    "url": "/ShlackChat/static/js/main.01e2adbe.chunk.js"
+  },
+  {
+    "revision": "caa2fccf88b8d47eb7f9",
+    "url": "/ShlackChat/static/js/runtime-main.a9afac1c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/ShlackChat/static/media/logo.5d5d9eef.svg"
+  }
+]);

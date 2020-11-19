7e26c12a20fa1a32feeb56ce4d12b027235cbbec
giorgioGunawan <grggnwn@gmail.com>
2020-11-19 23:32:07 +1100

    Your commit message

--- src/App.js
+++ src/App.js
@@ -1,16 +1,23 @@
-import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-
-import Dashboard from './Dashboard';
-import Store from './Store';
 
 function App() {
   return (
     <div className="App">
-      <Store>
-        <Dashboard />
-      </Store>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }

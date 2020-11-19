9a9ae51a8cd31361aadd8f5f545c6205f6df857d
giorgioGunawan <grggnwn@gmail.com>
2020-11-20 00:16:05 +1100

    Added structure of main components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,97 @@
-import logo from './logo.svg';
+import React from 'react';
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
 import './App.css';
 
+// Add useAuthentication hook from the react firebase hooks auth library
+import {useAuthState} from 'react-firebase-hooks/auth';
+
+// Add useState of useCollection data from the firestore library to be able 
+// to access collections within firestore (NOSQL)
+import {useCollectionData} from 'react-firebase-hooks/firestore';
+
+// from firebase - project settings adding webapp
+/*
+var firebaseConfig = {
+  apiKey: "AIzaSyBS7RMnNi3XEIDvMxGjhb1a7VIEQqO_Ufw",
+  authDomain: "giochat-160b7.firebaseapp.com",
+  projectId: "giochat-160b7",
+  storageBucket: "giochat-160b7.appspot.com",
+  messagingSenderId: "463084450667",
+  appId: "1:463084450667:web:1889f3ad8955d2b9f4d22e",
+  measurementId: "G-RZZ8F0F5PJ"
+};*/
+if (!firebase.apps.length) {
+  firebase.initializeApp({
+    apiKey: "AIzaSyBS7RMnNi3XEIDvMxGjhb1a7VIEQqO_Ufw",
+    authDomain: "giochat-160b7.firebaseapp.com",
+    projectId: "giochat-160b7",
+    storageBucket: "giochat-160b7.appspot.com",
+    messagingSenderId: "463084450667",
+    appId: "1:463084450667:web:1889f3ad8955d2b9f4d22e",
+    measurementId: "G-RZZ8F0F5PJ"
+  })
+}else {
+  firebase.app(); // if already initialized, use that one
+}
+
+
+const auth = firebase.auth();
+const firestore = firebase.firestore();
+
 function App() {
+
+  const [user] = useAuthState(auth);
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        
       </header>
+
+      <section>
+        {user ? <ChatRoom /> : <SignIn />}
+      </section>
     </div>
   );
+}
+
+function SignIn() {
+  const googleSignIn = () =>{
+    const provider = new firebase.auth.GoogleAuthProvider();
+
+    // triggers popup window when user signs in with google
+    auth.signInWithPopup(provider);
+  }
+  // listens to the click event of the button
+  return(
+    <button onClick={googleSignIn}> Google Authentication Sign In</button>
+  )
+}
+
+function SignOut(){
+  // if there is a user, use the firebase auth library sign out function
+  // on sign out button event
+  if (auth.currentUser){
+    return (<button onClick={() => auth.signOut()}>Sign Out</button>)
+  }
+}
+
+function ChatRoom() {
+  const messagesRef = firestore.collection('messages');
+  const query = messagesRef.orderBy('createdAt').limit(25);
+  const [messages] = useCollectionData(query, {idField: 'id'});
+  return(
+    <div>
+      {messages && messages.map(msg => <ChatMessage key={msg.id} message={msg} />)}
+    </div>
+  
+  )
+}
+
+function ChatMessage(props){
+  const {text, uid} = props.message;
+  return <p>{text}</p>
 }
 
 export default App;
